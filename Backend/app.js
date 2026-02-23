729104e74c7a93d9d576805ec86783268a603dcc
adityasingh6533 <aditya53020@gmail.com>
2026-02-23 16:26:59 +0530

    Fix update CORS origin for production frontend

--- Backend/app.js
+++ Backend/app.js
@@ -11,7 +11,7 @@ require("dotenv").config();
 const app = express();
 
 const corsOrigin = process.env.CORS_ORIGIN || "*";
-app.use(cors({ origin: corsOrigin === "*" ? true : corsOrigin.split(",") }));
+app.use(cors({ origin: process.env.CORS_ORIGIN , credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
